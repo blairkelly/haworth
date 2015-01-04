9a50ed6ee7e397df3a6b24980ba559eae14f5db4
Blair-MacMini <blairkelly1@gmail.com>
2015-01-04 12:37:35 -0500

    added some console logs

--- opencv_test.js
+++ opencv_test.js
@@ -1,6 +1,8 @@
 var cv = require('opencv');
 var fs = require('fs');
 
+console.log("\r\n");
+
 //var opencvBuffer = new Buffer(normalBuffer, 'binary');
 //opencv.readImage(opencvBuffer, function(err, image){})
 
@@ -14,4 +16,8 @@ cv.readImage("./test_in.jpg", function (err, im) {
         im.save('test_out.jpg');
     });
     console.log('sucessfully read.');
+
+    //console.log("\r\n\r\n");
+    //console.log(cv);
+    //console.log("\r\n\r\n");
 });
