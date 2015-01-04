c41df1a3956a518b78e61af1abc9046d902cdd0b
Blair-MacMini <blairkelly1@gmail.com>
2015-01-04 01:33:49 -0500

    opencv now working with test

---
+++ opencv_test.js
@@ -0,0 +1,17 @@
+var cv = require('opencv');
+var fs = require('fs');
+
+//var opencvBuffer = new Buffer(normalBuffer, 'binary');
+//opencv.readImage(opencvBuffer, function(err, image){})
+
+cv.readImage("./test_in.jpg", function (err, im) {
+    
+    im.detectObject(cv.FACE_CASCADE, {}, function (err, faces) {
+        for (var i=0;i<faces.length; i++){
+            var x = faces[i]
+            im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+        }
+        im.save('test_out.jpg');
+    });
+    console.log('sucessfully read.');
+});
