8ef3a4b1a6a39e7fccecb119865bff8fabe1b40a
Blair Kelly <blairkelly1@gmail.com>
2015-01-14 08:33:26 -0500

    videowindow tests

--- views/videotest/scripts.js
+++ views/videotest/scripts.js
@@ -2,8 +2,6 @@ var thedoc = $(document);
 var target = $('#hawvid');
 var vidstate = target.get(0);
 
-target.attr('width', thedoc.width());
-
 target.on('ended', function() {
     vidstate.play();
 });
@@ -12,4 +10,19 @@ socket.on('connect', function () {
     socket.on('refresh', function (data) {
         window.location.reload();
     });
+});
+
+
+
+
+var on_videowindow_resize = function () {
+    target.attr('width', thedoc.width()).attr('height', thedoc.height());
+}
+
+$(window).on('resize', function () {
+    on_videowindow_resize();
+});
+
+$(document).ready(function () {
+    on_videowindow_resize();
 });
