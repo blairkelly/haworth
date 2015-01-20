717f11321bf7dd4dd598598f4c22e1f78e3b90b7
Blair-MacMini <blairkelly1@gmail.com>
2015-01-19 21:33:14 -0500

    small change to config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {}
 config.thoughtbubbles = [
 ];
 
-config.serialaddress = "/dev/tty.usbmodem1471";
+config.serialaddress = "/dev/tty.usbmodem1481";
 
 //config.camera_ip = '10.0.1.222:3000';
 config.camera_ip = '10.0.1.23:3300';
