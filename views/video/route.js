71e15ca3c63608bbc3577f34f71b9f7be1a7aa60
Blair Kelly <blairkelly1@gmail.com>
2015-01-20 02:29:00 -0500

    more video

--- views/video/route.js
+++ views/video/route.js
@@ -2,7 +2,10 @@ var app = module.parent.exports.app;
 var io = module.parent.exports.io;
 
 app.get('/video', function (req, res) {
-	res.render('video/index.jade');
+    res.render('video/index1.jade');
+});
+app.get('/video1', function (req, res) {
+	res.render('video/index1.jade');
 });
 app.get('/video2', function (req, res) {
     res.render('video/index2.jade');
