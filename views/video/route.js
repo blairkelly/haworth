2ee3f46490de5a123fcffa539aae86abdd1bb9ff
Blair-MacMini <blairkelly1@gmail.com>
2015-01-19 19:17:45 -0500

    updated video route

--- views/video/route.js
+++ views/video/route.js
@@ -2,5 +2,5 @@ var app = module.parent.exports.app;
 var io = module.parent.exports.io;
 
 app.get('/video', function (req, res) {
-	res.render('videotest/index.jade');
+	res.render('video/index.jade');
 });
